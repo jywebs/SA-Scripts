71d8e55c9f4acf4ebed13bc15544765cd5e7ca15
JacyYork <jywebs@gmail.com>
2016-10-10 13:29:35 -0700

    update script to remove passwords

--- IDB_Scripts/DEMOs/get_uniq_logins.sh
+++ IDB_Scripts/DEMOs/get_uniq_logins.sh
@@ -1,8 +1,20 @@
 ﻿#!/bin/bash
 
+######################################
+##		get_demo_logs.sh			##
+######################################
+
+#Get current month
+if [ `date +'%d'` -lt "5" ]; then
+	_DATE_=`date +'%Y%m' -d '-1 month'`
+else
+	_DATE_=`date +'%Y%m'`
+fi
+
+#Get Login Data
 for i in mysql1 mssql1 oracle1; do
 	echo "User Report for $i";
-	ssh idb@$i -q "find /logs/201610*/idb.uilog.* | xargs egrep 'scalearc.com/api/login DATA=\"username' --text" | awk -F 'DATA=' '{print $2}'| sort | uniq -c;
+	ssh idb@$i -q "find /logs/201610*/idb.uilog.* | xargs egrep 'scalearc.com/api/login DATA=\"username' --text" | awk -F 'DATA=' '{print $2}'| awk '!/admin/ && !/root/ && !/adm/ {print 1}' | sort | uniq -c;
 done
 
 exit 0
