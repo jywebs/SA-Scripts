ab9361096072f3939d3890c0e3b9c739c0970e7b
JacyYork <jywebs@gmail.com>
2016-10-10 12:30:50 -0700

    demo_login_rpt.sh

---
+++ IDB_Scripts/DEMOs/get_uniq_logins.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+
+for i in mysql1 mssql1 oracle1; do
+	echo "User Report for $i";
+	ssh idb@$i -q "find /logs/201610*/idb.uilog.* | xargs egrep 'scalearc.com/api/login DATA=\"username' --text" | awk -F 'DATA=' '{print $2}'| sort | uniq -c;
+done
+
+exit 0
